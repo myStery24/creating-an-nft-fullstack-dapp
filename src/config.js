7e70c5d58a2086689d13c876e37964766c7893f3
ZiHui Lee <61684565+myStery24@users.noreply.github.com>
2023-06-17 06:30:57 +0800

    Upload files
    Submit quest 1

--- src/config.js
+++ src/config.js
@@ -4,38 +4,36 @@ const path = require("path");
 const isLocal = typeof process.pkg === "undefined";
 const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
 const { MODE } = require(path.join(basePath, "src/blendMode.js"));
-const description = "<your-stackup-username>'s PolyAliens NFT collection.";
-const baseUri = "ipfs://UPDATE";
+const description = "myStery24's PolyAliens NFT collection.";
+const baseUri = "ipfs://QmdYKaTEJ5FDwA6khYi85M4KGMTcFwzHLv1foEXD7mPzic";
 
-const layerConfigurations = [
-  {
+const layerConfigurations = [{
     growEditionSizeTo: 50,
     layersOrder: [
-      { name: "Background" },
-      { name: "Skin" },
-      { name: "Eye" },
-      { name: "Mouth" },
-      { name: "Shirt" },
-      { name: "Neck" },
-      { name: "Weed" },
-      { name: "Eyewear" },
-      { name: "Head" },
+        { name: "Background" },
+        { name: "Skin" },
+        { name: "Eye" },
+        { name: "Mouth" },
+        { name: "Shirt" },
+        { name: "Neck" },
+        { name: "Weed" },
+        { name: "Eyewear" },
+        { name: "Head" },
     ],
-  },
-];
+}, ];
 
 const shuffleLayerConfigurations = true;
 
 const debugLogs = false;
 
 const format = {
-  width: 600,
-  height: 600,
+    width: 600,
+    height: 600,
 };
 
 const background = {
-  generate: true,
-  brightness: "80%",
+    generate: true,
+    brightness: "80%",
 };
 
 const extraMetadata = {};
@@ -45,22 +43,22 @@ const rarityDelimiter = "#";
 const uniqueDnaTorrance = 10000;
 
 const preview = {
-  thumbPerRow: 15,
-  thumbWidth: 200,
-  imageRatio: format.width / format.height,
-  imageName: "collage.png",
+    thumbPerRow: 15,
+    thumbWidth: 200,
+    imageRatio: format.width / format.height,
+    imageName: "collage.png",
 };
 
 module.exports = {
-  format,
-  baseUri,
-  description,
-  background,
-  uniqueDnaTorrance,
-  layerConfigurations,
-  rarityDelimiter,
-  preview,
-  shuffleLayerConfigurations,
-  debugLogs,
-  extraMetadata,
+    format,
+    baseUri,
+    description,
+    background,
+    uniqueDnaTorrance,
+    layerConfigurations,
+    rarityDelimiter,
+    preview,
+    shuffleLayerConfigurations,
+    debugLogs,
+    extraMetadata,
 };
